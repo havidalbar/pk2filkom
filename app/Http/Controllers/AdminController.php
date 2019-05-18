95828b29abd22cc810dd0fb3fc8961cf28235772
havidalbar@gmail.com <havidalbar@gmail.com>
2019-05-18 21:09:52 +0700

    Menambah kondisi login tiap divisi dan Menambah fungsi crud artikel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Hash;
 use Validator;
+use App\Artikel;
 use App\Pengguna;
 use App\Mahasiswa;
 use App\NilaiKKM;
