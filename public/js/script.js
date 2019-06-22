3a508db5fd6284521b170e453f10ec34de643eae
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-06-23 06:50:07 +0700

    Memperbaiki height tiap section

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,43 @@
 $(document).ready(function() {    
-    $('.pk2-jb2').css('height',($(window).height() - 164)+'px');            
-    $('.pk2-jb3').css('height',($(window).height() - 364)+'px');      
-    $(window).bind('DOMContentLoaded load resize', function() {        
-        if($(window).width() >= 768) {            
-            $('.pk2-jb5').css('height',($(window).height() - 164)+'px');            
-            $('.pk2-jb6').css('height',($(window).height() - 164)+'px');           
-        }
-     });
     // Paralex fixNavbar
-    $(window).scroll(function(){
+    $(window).scroll(function() {
         let navScroll = $(this).scrollTop();
         // console.log(navScroll);
-        if(navScroll){
-            $('.nav-home').addClass('sticky-dekstop');
-            $('.nav-home').css({
-                'transition' : '1.5s'
+        if (navScroll) {
+            $(".nav-home").addClass("sticky-dekstop");
+            $(".nav-home").css({
+                transition: "1.5s"
             });
-        }else{
-            $('.nav-home').removeClass('sticky-dekstop');
+        } else {
+            $(".nav-home").removeClass("sticky-dekstop");
         }
     });
     // endParalex fixNavbar
 
     // ScrollAnimate
-    $('#swipUp, [data-item-ojb]').on('click', function(e){
+    $("#swipUp, [data-item-ojb]").on("click", function(e) {
         e.preventDefault();
-        if (this.id == 'swipUp') {
-            $('html, body').animate({
-                scrollTop : ($('.pk2-jb2').offset().top - (($(window).width() < 768 ) ? 160 : 165))
-            },1250,'swing');
+        if (this.id == "swipUp") {
+            $("html, body").animate(
+                {
+                    scrollTop:
+                        $(".pk2-jb2").offset().top -
+                        ($(window).width() < 768 ? 160 : 165)
+                },
+                1250,
+                "swing"
+            );
         } else {
-            let getObject = '.'+$(this).attr('data-item-ojb');
-            $('html, body').animate({
-                scrollTop : ($(getObject).offset().top - (($(window).width() < 768 ) ? 160 : 165))
-            },1250,'swing');
+            let getObject = "." + $(this).attr("data-item-ojb");
+            $("html, body").animate(
+                {
+                    scrollTop:
+                        $(getObject).offset().top -
+                        ($(window).width() < 768 ? 160 : 165)
+                },
+                1250,
+                "swing"
+            );
         }
     });
     // endScrollAnimate
