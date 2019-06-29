fc415a10d2defe1c2a0d1e91de57acd50416213e
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-06-29 09:54:38 +0700

    Benerin tambah soal

--- resources/views/panel-admin/tugas/form-create-edit-soal.blade.php
+++ resources/views/panel-admin/tugas/form-create-edit-soal.blade.php
@@ -5,7 +5,7 @@
         </div>
     </div>
     <div class="form-group m-form__group row align-items-center">
-        <label for="artikel-text-input" class="col-2 col-form-label">
+        <label class="col-2 col-form-label">
             Judul Tugas
         </label>
         <div class="col-10">
@@ -13,7 +13,7 @@
         </div>
     </div>
     <div class="form-group m-form__group row align-items-center">
-        <label for="artikel-text-input" class="col-2 col-form-label">
+        <label class="col-2 col-form-label">
             Deskripsi
         </label>
         <div class="col-10">
@@ -22,7 +22,7 @@
         </div>
     </div>
     <div class="form-group m-form__group row align-items-center">
-        <label for="artikel-text-input" class="col-2 col-form-label">
+        <label class="col-2 col-form-label">
             Waktu Mulai
         </label>
         <div class="col-10">
@@ -30,7 +30,7 @@
         </div>
     </div>
     <div class="form-group m-form__group row align-items-center">
-        <label for="artikel-text-input" class="col-2 col-form-label">
+        <label class="col-2 col-form-label">
             Waktu Akhir
         </label>
         <div class="col-10">
@@ -38,7 +38,7 @@
         </div>
     </div>
     <div class="form-group m-form__group row align-items-center">
-        <label for="artikel-text-input" class="col-2 col-form-label">
+        <label class="col-2 col-form-label">
             Random Tugas
         </label>
         <div class="col-10">
@@ -46,7 +46,7 @@
         </div>
     </div>
     <div class="form-group m-form__group row align-items-center">
-        <label for="artikel-text-input" class="col-2 col-form-label">
+        <label class="col-2 col-form-label">
             Batas Waktu
         </label>
         <div class="col-10">
@@ -55,85 +55,19 @@
         </div>
     </div>
     <input class="form-control m-input" name="tipe" type="hidden" value="pilihan_ganda">
-    <div class="border border-dark pt-3 pb-3 mt-3">
-        <div class="form-group m-form__group row align-items-center">
-            <label for="artikel-text-input" class="col-2 col-form-label">
-                Soal
-            </label>
-            <div class="col-10">
-                <input class="form-control m-input" name="soal[0][soal]" placeholder="Pertanyaan" type="text" required>
-            </div>
-        </div>
-        <div class="form-group m-form__group row align-items-center">
-            <label for="artikel-text-input" class="col-2 col-form-label">
-                Jawaban 0
-            </label>
-            <div class="col-10">
-                <input class="form-control m-input" name="soal[0][pilihan_jawaban][0]" placeholder="Jawaban 0"
-                    type="text" required>
-            </div>
-        </div>
-        <div class="form-group m-form__group row align-items-center">
-            <label for="artikel-text-input" class="col-2 col-form-label">
-                Jawaban 1
-            </label>
-            <div class="col-10">
-                <input class="form-control m-input" name="soal[0][pilihan_jawaban][1]" placeholder="Jawaban 1"
-                    type="text" required>
-            </div>
-        </div>
-        <div class="form-group m-form__group row align-items-center">
-            <label for="artikel-text-input" class="col-2 col-form-label">
-                Jawaban 2
-            </label>
-            <div class="col-10">
-                <input class="form-control m-input" name="soal[0][pilihan_jawaban][2]" placeholder="Jawaban 2"
-                    type="text" required>
-            </div>
-        </div>
-        <div class="form-group m-form__group row align-items-center">
-            <label for="artikel-text-input" class="col-2 col-form-label">
-                Jawaban 3
-            </label>
-            <div class="col-10">
-                <input class="form-control m-input" name="soal[0][pilihan_jawaban][3]" placeholder="Jawaban 3"
-                    type="text" required>
-            </div>
-        </div>
-        <div class="form-group m-form__group row align-items-center">
-            <label for="artikel-text-input" class="col-2 col-form-label">
-                Jawaban Benar
-            </label>
-            <div class="col-10">
-                <input class="form-control m-input" name="soal[0][jawaban_benar]"
-                    placeholder="Jawaban Mana Yang Benar Antara 0/1/2/3" type="number" min="0" max="3" required>
-            </div>
-        </div>
-    </div>
-    <div id="sub_konten_parent">
-        <div id="sub_konten_collector"></div>
-        <script src="https://cdn.ckeditor.com/ckeditor5/12.1.0/decoupled-document/ckeditor.js"></script>
-        <script src="{{ asset('js/CKEditorBarBar.js') }}"></script>
-        <script>
-        function getEditorData() {
-            let editors = document.getElementsByClassName('sub-konten-editor');
-            let sub_konten_collector = $('#sub_konten_collector');
-            sub_konten_collector.empty();
-            for (let i = 0; i < editors.length; i++) {
-                sub_konten_collector.append(
-                    `<input type="hidden" name="sub_konten[]" value="${editors[i].innerHTML}">`);
-            }
-            $('#submitter').trigger('click');
-        }
-
-        function tambahSubKonten() {
-            $('#sub_konten_parent').append(`<div class="border border-dark pt-3 pb-3 mt-3">
+    <div id="soal"></div>
+    <script>
+    var data = 0;
+    function tambahSoal() {
+        var nomorSoal = data+1;        
+        $('#soal').append(
+            `<div class="border border-dark pt-3 pb-3 mt-3">
                 <div class="form-group m-form__group row align-items-center">
                     <label for="artikel-text-input" class="col-2 col-form-label">
-                        Soal
+                        Soal `+nomorSoal+`
                     </label>
                     <div class="col-10">
-                        <input class="form-control m-input" name="soal[0][soal]" placeholder="Pertanyaan" type="text" required>
+                        <input class="form-control m-input" name="soal[`+data+`][soal]" placeholder="Pertanyaan" type="text" required>
                     </div>
                 </div>
                 <div class="form-group m-form__group row align-items-center">
@@ -141,7 +75,7 @@
                         Jawaban 0
                     </label>
                     <div class="col-10">
-                        <input class="form-control m-input" name="soal[0][pilihan_jawaban][0]" placeholder="Jawaban 0"
+                        <input class="form-control m-input" name="soal[`+data+`][pilihan_jawaban][0]" placeholder="Jawaban 0"
                             type="text" required>
                     </div>
                 </div>
@@ -150,7 +84,7 @@
                         Jawaban 1
                     </label>
                     <div class="col-10">
-                        <input class="form-control m-input" name="soal[0][pilihan_jawaban][1]" placeholder="Jawaban 1"
+                        <input class="form-control m-input" name="soal[`+data+`][pilihan_jawaban][1]" placeholder="Jawaban 1"
                             type="text" required>
                     </div>
                 </div>
@@ -159,7 +93,7 @@
                         Jawaban 2
                     </label>
                     <div class="col-10">
-                        <input class="form-control m-input" name="soal[0][pilihan_jawaban][2]" placeholder="Jawaban 2"
+                        <input class="form-control m-input" name="soal[`+data+`][pilihan_jawaban][2]" placeholder="Jawaban 2"
                             type="text" required>
                     </div>
                 </div>
@@ -168,7 +102,7 @@
                         Jawaban 3
                     </label>
                     <div class="col-10">
-                        <input class="form-control m-input" name="soal[0][pilihan_jawaban][3]" placeholder="Jawaban 3"
+                        <input class="form-control m-input" name="soal[`+data+`][pilihan_jawaban][3]" placeholder="Jawaban 3"
                             type="text" required>
                     </div>
                 </div>
@@ -177,43 +111,28 @@
                         Jawaban Benar
                     </label>
                     <div class="col-10">
-                        <input class="form-control m-input" name="soal[0][jawaban_benar]"
+                        <input class="form-control m-input" name="soal[`+data+`][jawaban_benar]"
                             placeholder="Jawaban Mana Yang Benar Antara 0/1/2/3" type="number" min="0" max="3" required>
                     </div>
                 </div>
             </div>`);
-            let subKontenEditor = document.getElementsByClassName('sub-konten-editor');
-            let subKontenEditorContainer = document.getElementsByClassName('sub-konten-editor-container');
-            DecoupledEditor
-                .create(subKontenEditor[subKontenEditor.length - 1], {
-                    extraPlugins: [MyCustomUploadAdapterPlugin],
-
-                    // ...
-                })
-                .then(editor => {
-                    subKontenEditorContainer[subKontenEditorContainer.length - 1].appendChild(editor.ui.view.toolbar
-                        .element);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }
-        </script>
-    </div>
+        data = data + 1;
+    }
+    </script>
     <div class="mt-3 mb-3" style="text-align: center">
-        <button onclick="tambahSubKonten()" class="btn btn-secondary" type="button">
+        <button onclick="tambahSoal()" class="btn btn-secondary" type="button">
             Tambah Soal
         </button>
     </div>
-    <div class="m-portlet__foot m-portlet__foot--fit">
+    <!-- <div class="m-portlet__foot m-portlet__foot--fit">
         <div class="m-form__actions">
-            <button onclick="getEditorData()" class="btn btn-primary" type="button">
+            <button onclick="#" class="btn btn-primary" type="button">
                 Simpan Tugas
             </button>
             <button type="reset" class="btn btn-secondary">
                 Reset
             </button>
         </div>
-    </div>
-    <button type="submit" id="submitter" style="display: none"></button>
+    </div> -->
+    <button type="submit" id="submitter">Submit</button>
 </div>
