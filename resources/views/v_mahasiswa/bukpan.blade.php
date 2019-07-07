532debd29d09470c6d96748d0aeedb80874965ae
kevkevin <nxfleuret@gmail.com>
2019-07-07 14:42:48 +0700

    desain awal bukpan

---
+++ resources/views/v_mahasiswa/bukpan.blade.php
@@ -0,0 +1,31 @@
+@extends ('layouts.template')
+@section('title', 'SiMaba! 2019 | FILKOM UB')
+
+@section('content')
+@include('layouts.header')
+<div class="jumbotron jumbotron-fluid bg-bukpan">
+    <div class="container bukpan">
+        <div class="card d-flex">
+            <div class="card title">
+                KETENTUAN CETAK BUKU PANDUAN
+            </div>
+            <div class="ketentuan">
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+                <p>1. Ketentuan 1.................................</p>
+            </div>
+            <div class="card title download">
+                DOWNLOAD BUKU PANDUAN
+            </div>
+        </div>
+    </div>
+</div>
+@include('layouts.footer')
+@endsection
