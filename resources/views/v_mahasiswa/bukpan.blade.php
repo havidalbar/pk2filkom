3e58d8dcc54f2bdbd4c5d0f9bbd319941bc78245
Havid Albar <havidalbar@Havids-MacBook-Pro.local>
2019-07-07 21:33:15 +0700

    Binding Sementara

--- resources/views/v_mahasiswa/bukpan.blade.php
+++ resources/views/v_mahasiswa/bukpan.blade.php
@@ -10,16 +10,11 @@
                 KETENTUAN CETAK BUKU PANDUAN
             </div>
             <div class="ketentuan">
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
-                <p>1. Ketentuan 1.................................</p>
+                <p>1. Periode: 29 Juli - 13 Agustus 2019</p>
+                <p>2. Buku dicetak dengan ukuran A5 dan dijilid langsung/ terusan/ lem</p>
+                <p>3. Bagian sampul dicetak berwarna dengan kertas buffalo putih dan dilaminasi</p>
+                <p>4. Bagian isi dicetak berwarna dengan kertas HVS secara bolak-balik</p>
+                <p>5. Buku panduan wajib dibawa pada setiap rangkaian PK2MABA & Startup Academy</p>
             </div>
             <div class="card title download">
                 DOWNLOAD BUKU PANDUAN
