2544bf009c1c360859b7298da549c987d9ca5e9f
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-07-12 02:22:49 +0700

    Update benerin tampilan tugas

--- resources/views/panel-admin/tugas/form-create-edit-pilgan.blade.php
+++ resources/views/panel-admin/tugas/form-create-edit-pilgan.blade.php
@@ -56,52 +56,52 @@
         </div>
     </div>
     <input class="form-control m-input" name="tipe" type="hidden" value="pilihan_ganda">
-    <div id="soal">
-        <div class="border border-dark pt-3 pb-3 mt-3">
-            <div class="form-group m-form__group row align-items-center">
-                <label class="col-2 col-form-label">
-                    Soal ${nomorSoal}
-                </label>
-                <div class="col-10">
-                    <input class="form-control m-input" name="soal[0][soal]" placeholder="Pertanyaan" type="text"
-                        required>
-                </div>
+    <div class="border border-dark pt-4 pb-4 mt-4 mb-4">
+        <div class="form-group m-form__group row align-items-center">
+            <label class="col-2 col-form-label">
+                Soal 1
+                <!-- ${nomorSoal} -->
+            </label>
+            <div class="col-10">
+                <input class="form-control m-input" name="soal[0][soal]" placeholder="Pertanyaan" type="text" required>
             </div>
-            <div class="form-group m-form__group row align-items-center">
-                <label class="col-form-label">
-                    Pilihan jawaban
-                </label>
-            </div>
-            <div class="form-group m-form__group row align-items-center">
+        </div>
+        <div class="form-group m-form__group row align-items-center">
+            <label class="col-form-label">
+                Pilihan jawaban
+            </label>
+        </div>
+        <div style="margin-left:24px">
+            <div class="form-group m-form__group d-flex flex-row align-items-center">
                 <input type="radio" name="jawaban_benar" value="0">
-                <div class="col-12">
+                <div style="width:100%;margin-left:16px">
                     <input class="form-control m-input" name="soal[0][pilihan_jawaban][0]"
-                        placeholder="Pilihan Jawaban 0" type="text" required>
-                </div>
-            </div>
-            <div class="form-group m-form__group row align-items-center">
-                <input type="radio" name="jawaban_benar" value="1">
-                <div class="col-12">
-                    <input class="form-control m-input" name="soal[0][pilihan_jawaban][1]"
                         placeholder="Pilihan Jawaban 1" type="text" required>
                 </div>
             </div>
-            <div class="form-group m-form__group row align-items-center">
-                <input type="radio" name="jawaban_benar" value="2">
-                <div class="col-12">
-                    <input class="form-control m-input" name="soal[0][pilihan_jawaban][2]"
+            <div class="form-group m-form__group d-flex flex-row align-items-center">
+                <input type="radio" name="jawaban_benar" value="1">
+                <div style="width:100%;margin-left:16px">
+                    <input class="form-control m-input" name="soal[0][pilihan_jawaban][1]"
                         placeholder="Pilihan Jawaban 2" type="text" required>
                 </div>
             </div>
-            <div class="form-group m-form__group row align-items-center">
-                <input type="radio" name="jawaban_benar" value="3">
-                <div class="col-12">
-                    <input class="form-control m-input" name="soal[0][pilihan_jawaban][3]"
+            <div class="form-group m-form__group d-flex flex-row align-items-center">
+                <input type="radio" name="jawaban_benar" value="2">
+                <div style="width:100%;margin-left:16px">
+                    <input class="form-control m-input" name="soal[0][pilihan_jawaban][2]"
                         placeholder="Pilihan Jawaban 3" type="text" required>
                 </div>
             </div>
+            <div class="form-group m-form__group d-flex flex-row align-items-center">
+                <input type="radio" name="jawaban_benar" value="3">
+                <div style="width:100%;margin-left:16px">
+                    <input class="form-control m-input" name="soal[0][pilihan_jawaban][3]"
+                        placeholder="Pilihan Jawaban 4" type="text" required>
+                </div>
+            </div>
         </div>
-    </div>
+	</div>	
     <div class="m-portlet__foot m-portlet__foot--fit">
         <div class="m-form__actions">
             <button onclick="#" class="btn btn-primary" type="button">
