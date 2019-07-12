7f21f3c6fe4eb6be472b0cc7b5af7b2cf6da870b
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-07-12 19:00:14 +0700

    Nambah icon dan tooltip

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,11 @@
 @section('title', 'SiMaba! 2019 | FILKOM UB')
 
 @section('content')
+<script>
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+</script>
 <div class="jumbotron jumbotron-fluid bg-login">
     <div class="container d-flex flex-column align-items-center justify-content-center">
         <div>
@@ -11,10 +16,25 @@
             <form method="POST">
                 @csrf
                 <div class="form-group">
-                    <input type="number" class="form-control" name="nim" placeholder="Masukkan NIM">
+                    <label class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text" data-toggle="tooltip" data-placement="left" title="Gunakan Akun SIAM Untuk Login">
+                                <i class="far fa-user-alt"></i>
+                            </div>
+                        </div>
+                        <input type="number" class="form-control" name="nim" placeholder="Masukkan NIM" required>
+                    </label>
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Masukkan Kata Sandi">
+                    <label class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <i class="far fa-lock-alt"></i>
+                            </div>
+                        </div>
+                        <input type="password" class="form-control" name="password" placeholder="Masukkan Kata Sandi"
+                            required>
+                    </label>
                 </div>
                 <div class="d-flex justify-content-center">
                     <button type="submit" class="btn btn-login">Masuk</button>
