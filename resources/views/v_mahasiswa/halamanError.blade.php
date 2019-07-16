0313661a21c74ea093babaa8080d67e9aedd67fe
havidalbar@gmail.com <havidalbar@gmail.com>
2019-07-16 15:57:34 +0700

    Menambah halaman error handle

---
+++ resources/views/v_mahasiswa/halamanError.blade.php
@@ -0,0 +1,17 @@
+@extends ('layouts.template')
+@section('title', 'SiMaba! 2019 | FILKOM UB')
+
+@section('content')
+<div class="jumbotron jumbotron-fluid bg-halError">
+    <div class="container error">
+        <div class="textError">
+            <p class="errormsg">404</p>
+            <p>Page Not Found</p>
+            <p>Belum move on? Halaman ini aja udah :)</p>
+        </div>
+        <div class="card home">
+            HOME
+        </div>
+    </div>
+</div>
+@endsection
